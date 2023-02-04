51264f58d9a8cb67fffde48247a6ee9bd74b04ec
pranaavs2003 <pranaavs2003@gmail.com>
2023-02-04 10:41:44 +0530

    Email Service Added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./contact.scss";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
   const [isSubmit, setIsSubmit] = useState(false);
@@ -11,6 +12,55 @@ export default function Contact() {
   const quinprocEmail = "info@quinproc.co.in";
   const emailSubject = "Test Subject";
   const emailBody = "Test Body";
+  const form = useRef();
+
+  // const sendEmail = () => {
+  //   //e.preventDefault();
+  //   const emailData = {
+  //     name: name,
+  //     email: email,
+  //     number: number,
+  //     message: message,
+  //   };
+
+  //   emailjs
+  //     .sendForm(
+  //       "service_2gsidbs",
+  //       "template_7mam6gn",
+  //       emailData,
+  //       "YOUR_PUBLIC_KEY"
+  //     )
+  //     .then(
+  //       (result) => {
+  //         console.log(result.text);
+  //       },
+  //       (error) => {
+  //         console.log(error.text);
+  //       }
+  //     );
+  // };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_5mv34e6",
+        "template_gwnsfmn",
+        form.current,
+        "CcsC8HqyajUV4cZr-"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    //form.reset();
+  };
 
   return (
     <div className="contact">
@@ -25,58 +75,55 @@ export default function Contact() {
           method="post"
           enctype="text/plain"
         > */}
-        <input
-          type="text"
-          className="input__box"
-          placeholder="Name"
-          name="name"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          className="input__box"
-          placeholder="Email ID"
-          name="emailid"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="text"
-          className="input__box"
-          placeholder="Contact Number"
-          name="contactnumber"
-          onChange={(e) => setNumber(e.target.value)}
-        />
-        <input
-          type="text"
-          className="input__box"
-          placeholder="Message"
-          name="message"
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button
-          className="submit__button"
-          type="submit"
-          value="Send"
-          onClick={() => setIsSubmit(true)}
-        >
-          {/* <a
-            href="mailto:`{testEmail}`?subject={emailSubject}&body={emailBody}"
-            to="#"
-            className="link"
-          > */}
-          Submit
-          {/* </a> */}
-        </button>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            className="input__box"
+            placeholder="Name"
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            className="input__box"
+            placeholder="Email ID"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            className="input__box"
+            placeholder="Contact Number"
+            name="number"
+            onChange={(e) => setNumber(e.target.value)}
+          />
+          <input
+            type="text"
+            className="input__box"
+            placeholder="Message"
+            name="message"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button
+            className="submit__button"
+            type="submit"
+            value="Send"
+            onClick={() => {
+              setIsSubmit(true);
+              sendEmail();
+            }}
+          >
+            Submit
+          </button>
+        </form>
         {isSubmit && (
           <div
             className="form__submit"
             style={{ color: " rgb(235, 184, 184)" }}
           >
             Your form has been submitted successfully.
-            {/* {name + email + number + message} */}
           </div>
         )}
-        {/* </form> */}
       </div>
     </div>
   );
