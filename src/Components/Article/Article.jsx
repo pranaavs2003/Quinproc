3c1cb37b6cd12c107cb8d47b3aa30b793c9b9c62
pranaavs2003 <pranaavs2003@gmail.com>
2024-01-24 00:40:26 +0530

    Update Commit

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -6,6 +6,17 @@ export default function Article() {
     <div className="article">
       <div className="left__container">
         Our Innovations have won numerous Awards & Recognition
+        <div className="cert" >
+          
+          <div className="cert_logo" >
+            <img src="https://res.cloudinary.com/dbzzj25vc/image/upload/v1699255758/Quinproc/vcf6pdsbwxsqufrufrpz.png" alt="DPIIT-cert" />
+          </div>
+
+          <div className="text" >
+          We are recognized by the Department for Promotions of Industry and Internal Trade
+          </div>
+
+        </div>
       </div>
       <div className="right__container">
         <div className="news__box">
